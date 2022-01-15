f14dbf4c5a9b4aa3e3dcde41e03ec731d776d13b
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-01-15 18:26:32 +0100

    theme: migrate to sphinx-book-theme
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- sphinx_ncs_theme/__init__.py
+++ sphinx_ncs_theme/__init__.py
@@ -1,10 +1,14 @@
 import os
 
+import sphinx_book_theme
+
 
 __version__ = "0.7.0"
 
 
 def setup(app):
+    sphinx_book_theme.setup(app)
+
     app.add_html_theme(
         "sphinx_ncs_theme", os.path.dirname(os.path.abspath(__file__))
     )
