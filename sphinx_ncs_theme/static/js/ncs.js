3470b12fb8cc346e1e4d0c7187e589a7efa95ea9
Gerard Marull-Paretas <gerard@teslabs.com>
2023-03-23 18:36:55 +0100

    doc: remove unused components version code
    The code, while executed, is no longer used by the new theme. Drop this
    dead code.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- sphinx_ncs_theme/static/js/ncs.js
+++ sphinx_ncs_theme/static/js/ncs.js
@@ -29,30 +29,21 @@ function NCS () {
   };
 
   /*
-   * Find the index of the first element that matches a X.Y.Z release, this
-   * is considered the last "official" release.
-   */
-  state.findLastVersionIndex = function() {
-    const versions = window.NCS.versions.VERSIONS;
-    window.NCS.last_version_index = 1;
-    for (var index in versions) {
-      if (STABLE_VERSION_RE.test(versions[index])) {
-        window.NCS.last_version_index = index;
-        break;
-      }
-    }
-  }
-
-  /*
    * Infer the currently running version of the documentation
    */
   state.findCurrentVersion = function() {
     const path = window.location.pathname;
     if (path.startsWith(this.url_prefix)) {
       const prefix_len = this.url_prefix.length;
-      window.NCS.current_version = path.slice(prefix_len).split("/")[0];
+      window.NCS.current_version_text = path.slice(prefix_len).split("/")[0];
+      if (window.NCS.current_version_text === "latest") {
+        window.NCS.current_version = window.NCS.versions[0];
+      } else {
+        window.NCS.current_version = window.NCS.current_version_text;
+      }
     } else {
-      window.NCS.current_version = "latest";
+      window.NCS.current_version_text = "latest";
+      window.NCS.current_version = window.NCS.versions[0];
     }
   };
 
@@ -63,7 +54,7 @@ function NCS () {
    */
   state.findCurrentPage = function() {
     const path = window.location.pathname;
-    const version_prefix = window.NCS.current_version + "/";
+    const version_prefix = window.NCS.current_version_text + "/";
     if (path.startsWith(this.url_prefix)) {
       const prefix_len = this.url_prefix.length;
       let new_page = path.slice(prefix_len);
@@ -83,18 +74,15 @@ function NCS () {
    */
   state.updateVersionDropDown = function() {
     const ncs = window.NCS;
-    const versions = ncs.versions.VERSIONS;
-    const by_version = ncs.versions.COMPONENTS_BY_VERSION[ncs.current_version] ||
-                       ncs.versions.COMPONENTS_BY_VERSION["latest"];
 
     // Update dropdown text
-    const ncs_version = by_version["ncs"];
-    $("#ncsversion").text(`v${ncs_version}`);
+    $("#ncsversion").text(`v${ncs.current_version}`);
 
     // Update dropdown content
-    $.each(versions, function(_, v) {
+    $.each(ncs.versions, function(i, v) {
       if (v !== ncs.current_version) {
-        let link = `<a class="dropdown-item" href=${ncs.url_root + v}/${ncs.current_page}>${v}</a>`;
+        let rv = i === 0 ? "latest" : v;
+        let link = `<a class="dropdown-item" href=${ncs.url_root + rv}/${ncs.current_page}>${rv}</a>`;
         $("div.dropdown-menu").append(link);
       }
     });
@@ -106,14 +94,13 @@ function NCS () {
    */
   state.showVersion = function() {
     const ncs = window.NCS;
-    const VERSIONS = ncs.versions.VERSIONS;
-    const last_version_index = ncs.last_version_index;
+    const last_version = ncs.versions[1];
     const path_suffix = "/" +  ncs.current_page;
-    const last_release_url = ncs.url_root + VERSIONS[last_version_index] + path_suffix;
+    const last_release_url = ncs.url_root + last_version + path_suffix;
     const latest_release_url = ncs.url_root + "latest" + path_suffix;
 
     const SWITCH_MSG = "You might want to switch to the documentation for " +
-      "the <a href='" + last_release_url + "'>" + VERSIONS[last_version_index] +
+      "the <a href='" + last_release_url + "'>" + last_version +
       "</a> release or the <a href='" + latest_release_url + "'>current " +
       "state of development</a>."
 
@@ -126,14 +113,14 @@ function NCS () {
     const LAST_RELEASE_MSG =
       "You are looking at the documentation for the latest official release.";
 
-    if (ncs.current_version === VERSIONS[last_version_index]) {
+    if (ncs.current_version === last_version) {
       $("div.announcement").html(LAST_RELEASE_MSG);
       $("div.announcement").show();
     } else if (DEV_VERSION_RE.test(ncs.current_version)) {
       $("div.announcement").html(DEV_RELEASE_MSG);
       $("div.announcement").show();
-    } else if (VERSIONS.includes(ncs.current_version) &&
-               ncs.current_version !== VERSIONS[0]) {
+    } else if (ncs.versions.includes(ncs.current_version) &&
+               ncs.current_version !== ncs.versions[0]) {
       $("div.announcement").html(OLD_RELEASE_MSG);
       $("div.announcement").show();
     }
@@ -141,7 +128,6 @@ function NCS () {
 
   state.updatePage = function() {
     let ncs = window.NCS;
-    ncs.findLastVersionIndex();
     ncs.findCurrentVersion();
     ncs.findCurrentPage();
     ncs.updateVersionDropDown();
